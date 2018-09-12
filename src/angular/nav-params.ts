832e2242bd96fa8c59fe0048801223632b5abf68
Libin Varghese <libinv@gmail.com>
2018-09-12 14:35:03 +0530

    feat(NavParams): allow multiple params

--- src/angular/nav-params.ts
+++ src/angular/nav-params.ts
@@ -6,7 +6,8 @@ const METHODS = ['get'];
 export class NavParamsMock extends BaseMock {
     constructor(getReturn?: any) {
         super('NavParams', METHODS);
-        this.setReturn('get', getReturn);
+
+        this.spyObj.get.and.callFake(param => getReturn ? getReturn[param] : getReturn);
     }
 
     @deprecated('new NavParamsMock()')
