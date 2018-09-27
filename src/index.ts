c5ec8fc4ad70d81596c2323699e6617d93977a32
Libin Varghese <libinv@gmail.com>
2018-09-27 21:35:40 +0530

    feat(ionic-native/app-rate): added mock

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './native';
 export * from './angular';
 export * from './ionic';
+export * from './ionic-native';
---
+++ src/ionic-native/app-rate.ts
@@ -0,0 +1,22 @@
+import { BaseMock } from '../base.mock';
+import deprecated from 'deprecated-decorator';
+
+const METHODS = ['promptForRating', 'navigateToAppStore'];
+
+export class AppRateMock extends BaseMock {
+    constructor() {
+        super('AppRate', METHODS);
+    }
+
+    get preferences() {
+        return {
+          storeAppURL: {},
+          callbacks: {}
+        };
+    }
+
+    @deprecated('new AppRate()')
+    public static instance(): any {
+        return new AppRateMock();
+    }
+}
