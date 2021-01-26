0dc824be08fc6494db08d5542df87f2678f7ef87
Libin Varghese <libinv@gmail.com>
2021-01-27 01:21:08 +0530

    test: add nyc for coverage

---
+++ nyc.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  extends: '@istanbuljs/nyc-config-typescript',
+  reporter: ['lcov', 'text', 'text-summary'],
+  all: true,
+  exclude: [
+    '**/*.spec.ts',
+    'dist/*',
+    'coverage/*',
+    '*.js',
+    'scripts/*',
+    'eslintrc/*',
+    'spec/helpers/*'
+  ],
+  statements: 95,
+  branches: 95,
+  functions: 90,
+  lines: 95,
+};
